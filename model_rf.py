e1dc83c7ea65ff58fc6c93038bd74d663bb343d0
MahsaFarnia23 <143577665+MahsaFarnia23@users.noreply.github.com>
2025-08-18 20:02:25 -0400

    Update model_rf.py

--- model_rf.py
+++ model_rf.py
@@ -9,9 +9,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 import seaborn as sns
+from lime import lime_tabular
 import matplotlib.pyplot as plt
 import joblib
-from lime import lime_tabular
 
 def main():
     # Load the dataset
